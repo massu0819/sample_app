019b6ca11f5e0ce7caa13bde7bc54e589ff4c4e6
massu0819 <massu4869@gmail.com>
2021-10-10 02:28:48 +0000

    [Finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'edd2c784d72f640ae3f93008f222f3d32322cd5eef70293583cd126a395b8019ced83a14b7bbbfe9a81668f9224f3c517b8222154f7b74d0ae3f885d7b602b52'
